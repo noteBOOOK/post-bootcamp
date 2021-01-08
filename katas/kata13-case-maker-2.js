9c1bb176594dd1814e7bc703553408ce55644a46
noteBOOOK <jason.long77@hotmail.com>
2021-01-08 16:30:32 -0500

    Set priority of cases, loops through input cases for the priority

--- katas/kata13-case-maker-2.js
+++ katas/kata13-case-maker-2.js
@@ -13,9 +13,33 @@ const camelCase = function(input) {
 }
 
 const makeCase = (input, inputCase) => {
-  
+  let result = "";
+  // Priority of cases
+  const priority = {
+    first: ["camel", "pascal", "snake", "kebab", "title"],
+    second: ["vowel", "consonant"],
+    third: ["upper", "lower"],
+  }
+
+  let order = {
+    first: [],
+    second: [],
+    third: [],
+  };
+  // Set inputCase as an array if not already
+  Array.isArray(inputCase) === true ? parsedInputCase = inputCase : parsedInputCase = [inputCase]
+  // Loop through inputCase array and sort cases by priority
+  for (let i of parsedInputCase) {
+    if (priority.first.includes(i)){
+      order.first.push(i);
+    } else if (priority.second.includes(i)){
+      order.second.push(i);
+    } else {
+      order.third.push(i);
+    }
+  }
+  console.log(order);
 }
 
-
-console.log(camelCase("this is a string"));
+makeCase("this is a string", ["camel", "snake", "upper", "vowel", "lower", "consonant", "title"]);
 
