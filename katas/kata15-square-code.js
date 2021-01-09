f3431cb8e5a47536118e944a708bdc48037b2ca7
noteBOOOK <jason.long77@hotmail.com>
2021-01-09 16:41:20 -0500

    Implemented square of message

--- katas/kata15-square-code.js
+++ katas/kata15-square-code.js
@@ -1,5 +1,24 @@
 const squareCode = function(message) {
 
+  let result = "";
+  const string = message.split(" ");
+  const stringLength = string.join("").length;
+  const square = Math.ceil(Math.sqrt(stringLength));
+
+  return square;
 };
 
 console.log(squareCode("chill out"));
+
+/*
+chi
+llo
+ut
+
+clu
+hlt
+io
+
+clu hlt io
+
+*/
