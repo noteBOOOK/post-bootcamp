0a858ce26d682cccbc025dc45990a6dd528ac22f
noteBOOOK <jason.long77@hotmail.com>
2021-01-11 16:19:46 -0500

    Added queen pieces to the board

--- katas/kata16-queen-threat-detector.js
+++ katas/kata16-queen-threat-detector.js
@@ -3,7 +3,9 @@ const generateBoard = (whiteQueen, blackQueen) => {
   for (let i = 0; i < 8; i++) {
     result.push([0, 0, 0, 0, 0, 0, 0, 0]);
   }
+  result[whiteQueen[0]][whiteQueen[1]] = 1;
+  result[blackQueen[0]][blackQueen[1]] = 1;
   return result;
 };
 
-console.log(generateBoard(1, 2));
+console.log(generateBoard([0, 0], [1, 1]));
