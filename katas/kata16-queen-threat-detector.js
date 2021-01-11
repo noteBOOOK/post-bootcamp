86bbb9f01bcbd0ceef3afaad6271efc498301cc8
noteBOOOK <jason.long77@hotmail.com>
2021-01-11 16:33:07 -0500

    Added queen threat function, finds positions of queens

--- katas/kata16-queen-threat-detector.js
+++ katas/kata16-queen-threat-detector.js
@@ -1,3 +1,6 @@
+let whiteQueen = [0, 5];
+let blackQueen = [5, 0];
+
 const generateBoard = (whiteQueen, blackQueen) => {
   let result = [];
   for (let i = 0; i < 8; i++) {
@@ -8,4 +11,22 @@ const generateBoard = (whiteQueen, blackQueen) => {
   return result;
 };
 
-console.log(generateBoard([0, 0], [1, 1]));
+let generatedBoard = generateBoard(whiteQueen, blackQueen);
+
+const queenThreat = filledBoard => {
+  let queenPositions = [];
+
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+
+      if (filledBoard[i][j] === 1) {
+        queenPositions.push([i, j])
+      }
+
+    }
+  }
+  return queenPositions;
+};
+
+console.log(generatedBoard);
+console.log(queenThreat(generatedBoard));
