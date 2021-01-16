6ec1a52f18251255276cabdaaa26cd91672a1ae1
noteBOOOK <jason.long77@hotmail.com>
2021-01-16 14:46:24 -0500

    Finished time conversion challenge

--- HackerRank/Basic Problem Solving/09-time-conversion.js
+++ HackerRank/Basic Problem Solving/09-time-conversion.js
@@ -3,7 +3,15 @@ const inputData = "07:05:45PM";
 const timeConversion = input => {
   let AMPM = input.slice(-2);
 
-  console.log(AMPM);
+  let time = input.slice(0, -2).split(":");
+
+  if (AMPM === "AM" && time[0] === "12") {
+    time[0] = "00";
+  } else if (AMPM === "PM") {
+    time[0] = (time[0] % 12) + 12;
+  }
+  
+  return(time.join(":"));
 
 }
 
