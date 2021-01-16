8251a3e4425d626237b20bde0ff486ef558e679f
noteBOOOK <jason.long77@hotmail.com>
2021-01-16 14:16:14 -0500

    started birthday cake candles challenge

---
+++ HackerRank/Basic Problem Solving/08-birthday-cake-candles.js
@@ -0,0 +1,10 @@
+const inputData = [3, 2, 1, 3];
+
+const birthdayCakeCandles = candles => {
+  let resultCount = 0;
+
+  sortedCandles = candles.sort((a, b) => a - b);
+  console.log(sortedCandles);
+};
+
+birthdayCakeCandles(inputData);
