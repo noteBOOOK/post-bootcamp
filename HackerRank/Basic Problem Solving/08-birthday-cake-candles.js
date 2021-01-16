645d6d30b9d2f7616801ed2295155c4a1c2e1bee
noteBOOOK <jason.long77@hotmail.com>
2021-01-16 14:26:38 -0500

    Added comments

--- HackerRank/Basic Problem Solving/08-birthday-cake-candles.js
+++ HackerRank/Basic Problem Solving/08-birthday-cake-candles.js
@@ -1,10 +1,11 @@
 const inputData = [3, 2, 1, 3];
 
 const birthdayCakeCandles = candles => {
-
+  // Sort candles by ascending order and find tallest candle
   const sortedCandles = candles.sort((a, b) => a - b);
   const maxHeight = sortedCandles[sortedCandles.length - 1];
 
+  // Filter all candles to see which match the max height
   const tallestCandles = sortedCandles.filter(candle => candle === maxHeight);
   return tallestCandles.length;
 };
