dc9acd26d9f028b8ef4b0f51b1fbc4b3dd05faec
noteBOOOK <jason.long77@hotmail.com>
2021-01-16 14:10:06 -0500

    Finished mini max sum challenge

--- HackerRank/Basic Problem Solving/07-mini-max-sum.js
+++ HackerRank/Basic Problem Solving/07-mini-max-sum.js
@@ -1,8 +1,17 @@
 const inputData = [3, 1, 5, 9, 7];
 
 const miniMaxSum = arr => {
+  let sum1 = 0;
+  let sum2 = 0;
   arr.sort((a, b) => a - b);
-  console.log(arr);
+  for (let i = 0; i < arr.length - 1; i++) {
+    sum1 += arr[i];
+  }
+
+  for (let i = arr.length - 1; i > 0; i--) {
+    sum2 += arr[i];
+  }
+  console.log(sum1, sum2);
 };
 
 miniMaxSum(inputData);
