3cdd36875b3e0e8c6edeedb2e471f81ff9121e47
noteBOOOK <jason.long77@hotmail.com>
2021-01-18 16:09:54 -0500

    Finished grading students challenge

--- HackerRank/Basic Problem Solving/10-grading-students.js
+++ HackerRank/Basic Problem Solving/10-grading-students.js
@@ -1,6 +1,17 @@
 const data = [73, 67, 38, 33];
 
 const gradingStudents = grades => {
-  
+  const results = [];
+
+  grades.map(grade => {
+    const difference = 5 - (grade % 5);
+
+    if (difference < 3 && grade >= 38) {
+      grade += difference;
+    }
+    console.log(grade);
+  })
 
 };
+
+gradingStudents(data);
