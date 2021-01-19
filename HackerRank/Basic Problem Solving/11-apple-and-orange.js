6380d45aa22adf3d3e7aca4a2326d69e32f9bc29
noteBOOOK <jason.long77@hotmail.com>
2021-01-19 17:56:35 -0500

    finished apple and orange challenge

--- HackerRank/Basic Problem Solving/11-apple-and-orange.js
+++ HackerRank/Basic Problem Solving/11-apple-and-orange.js
@@ -7,4 +7,26 @@ const orangesArray = [5, -6];
 
 const countApplesAndOranges = (s, e, a, o, apples, oranges) => {
 
+  const result = {
+    appleCount: 0,
+    orangeCount: 0,
+  };
+
+  apples.forEach(apple => {
+    apple += a;
+    if (apple >= s && apple <= e) {
+      result.appleCount += 1;
+    }
+  })
+
+  oranges.forEach(orange => {
+    orange += o;
+    if (orange <= e && orange >= s) {
+      result.orangeCount += 1;
+    }
+  })
+  console.log(result.appleCount);
+  console.log(result.orangeCount);
 }; 
+
+countApplesAndOranges(start, end, appleTree, orangeTree, applesArray, orangesArray);
