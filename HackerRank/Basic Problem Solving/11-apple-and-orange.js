aa3200155fa7a453aec63b1200571fd8a336b968
noteBOOOK <jason.long77@hotmail.com>
2021-01-19 17:41:52 -0500

    Started apple and orange challenge

---
+++ HackerRank/Basic Problem Solving/11-apple-and-orange.js
@@ -0,0 +1,10 @@
+const start = 7;
+const end = 11;
+const appleTree = 5;
+const orangeTree = 15;
+const applesArray = [-2, 2, 1];
+const orangesArray = [5, -6];
+
+const countApplesAndOranges = (s, e, a, o, apples, oranges) => {
+
+}; 
